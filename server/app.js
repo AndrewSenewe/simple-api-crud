82907d752996248d9a2421d5bd666dd5d6a5d418
AndrewSenewe <sindrew.christ@gmail.com>
2017-07-06 10:39:56 +0700

    adding mongodb connection

--- server/app.js
+++ server/app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 var foods = require('./routes/foods.js');
 var restaurants = require('./routes/restaurants.js');
 
 var app = express();
+mongoose.connect('mongodb://localhost/andrew-restaurant')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/foods', foods);
 app.use('/restaurants', restaurants);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
